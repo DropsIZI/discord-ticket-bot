96d6ce141b3b63a0d2b20676bcb623fd8b6b2fb9
DropsIZI <Padroas70@gmail.com>
2026-05-09 13:12:36 -0400

    Update modpack download link for premium and free

--- commands/setupInstalacionFree.js
+++ commands/setupInstalacionFree.js
@@ -31,7 +31,7 @@ module.exports = {
         `> ⚠️ **Si tienes AdBlock activado, la página no te dejará descargar.** Desactívalo temporalmente.\n\n` +
         `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n` +
         `## 📦 PASO 2 — Descarga el Modpack de Cobbleverse\n` +
-        `> https://drive.google.com/file/d/1X-5VbHEgoKuhUF-KnBjLNZ6cKqmFJmRo/view?usp=sharing\n\n` +
+        `> https://drive.google.com/file/d/1SsU0bC7D8At3bNfb9Xl6ZcjpQvgKVCyu/view?usp=sharing\n\n` +
         `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n` +
         `## ⚙️ PASO 3 — Configura SKLauncher\n` +
         `> Abre **SKLauncher** y añade tu **Nickname**.\n` +
