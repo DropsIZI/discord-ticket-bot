3d1b04c38f3d7418f7e4e1eb1350242e0d25dd78
DropsIZI <Padroas70@gmail.com>
2026-05-09 00:05:06 -0400

    Update modpack download link for premium and free

--- commands/setupInstalacion.js
+++ commands/setupInstalacion.js
@@ -44,7 +44,7 @@ module.exports = {
         `Descárgalo en: https://modrinth.com/app\n\n` +
         `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n` +
         `## 📥 Link de descarga del Modpack (Premium)\n` +
-        `> https://drive.google.com/file/d/1h4yuDkA2qGS0ym_mc8U0WuoMu-kd10Nv/view?usp=sharing\n\n` +
+        `> https://drive.google.com/file/d/1X-5VbHEgoKuhUF-KnBjLNZ6cKqmFJmRo/view?usp=sharing\n\n` +
         `❓ ¿Tienes problemas? Abre un ticket en <#1486773607542554864>`
       )
       .setFooter({ text: `${THEME.footer}` })
